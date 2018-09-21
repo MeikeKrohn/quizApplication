84dc7eca11d58fdfc0e6a37671b374b627624905
MeikeKrohn <mk261@hdm-stuttgart.de>
2018-09-21 08:57:38 +0300

    Added basic app skeleton

---
+++ src/Controller/QuizController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meike
+ * Date: 20.09.2018
+ * Time: 18:41
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class QuizController extends AbstractController
+{
+    public function home()
+    {
+        return $this->render('home.html.twig');
+    }
+}
