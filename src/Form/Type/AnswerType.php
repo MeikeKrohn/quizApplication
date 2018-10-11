18ef5062ef88ae7611796f877373df3fc1d530c7
MeikeKrohn <mk261@hdm-stuttgart.de>
2018-10-11 10:15:40 +0300

    Fixed delete existing Answers Buttons Buttons appear now correctly next to each answer in the editQuestion-View

--- src/Form/Type/AnswerType.php
+++ src/Form/Type/AnswerType.php
@@ -11,9 +11,11 @@ namespace App\Form\Type;
 
 use App\Entity\Answer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Button;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
@@ -21,6 +23,16 @@ class AnswerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+
+        /*
+        $builder->add('delete', ButtonType::class,
+            array(
+                'attr' => array(
+                    'class' => 'deleteExistingAnswerButton')
+            ));
+        */
+
         $builder->add('answerText');
         $builder->add('isCorrect', ChoiceType::class,
             array(
@@ -29,7 +41,18 @@ class AnswerType extends AbstractType
                     'no' => false),
                 'multiple' => false, 'expanded' => true
             ));
-        //$builder->add('id', HiddenType::class);
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $data = $event->getData();
+            $form = $event->getForm();
+
+            if($data != null) {
+                $form->add('delete', ButtonType::class, array(
+                    'attr' => array('data-id' => $data->getId(), 'class' => 'deleteExistingAnswerButton')
+                ));
+            }
+        });
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
